614db4a88881486f7497a18f99e078d8dc9e8dba
Shivam Kumar <shivamkumar250403@gmail.com>
2025-02-27 23:20:20 +0530

    0

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,17 @@ const path = require('path');
 module.exports = {
   resolve: {
     fallback: {
-      path: require.resolve('path-browserify')
+      "fs": false,
+        "tls": false,
+        "net": false,
+        "path": false,
+        "zlib": false,
+        "http": false,
+        "https": false,
+        "stream": false,
+        "crypto": false,
+        "crypto-browserify": require.resolve('crypto-browserify'),
+      // path: require.resolve('path-browserify')
     }
   }
 };
