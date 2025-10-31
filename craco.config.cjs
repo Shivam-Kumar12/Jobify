23f1ebb06aefb27b76e03a5f379a329b69af5a5d
Shivam Kumar <shivamkumar250403@gmail.com>
2025-10-31 23:53:14 +0530

    Fix webpack polyfills for path, os, and crypto modules

---
+++ craco.config.cjs
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        path: require.resolve('path-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        crypto: require.resolve('crypto-browserify'),
+      };
+      return webpackConfig;
+    },
+  },
+};
